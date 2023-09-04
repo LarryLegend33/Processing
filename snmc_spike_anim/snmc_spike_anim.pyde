ebdf51a57ef49932ca06bbd2a6a1b5c401f37590
larrylegend33 <andrewdbolton@fas.harvard.edu>
2023-09-04 17:14:07 -0400

    Animation is running w/ no errors

--- snmc_spike_anim/snmc_spike_anim.pyde
+++ snmc_spike_anim/snmc_spike_anim.pyde
@@ -6,45 +6,40 @@
 # A nice sampler can be made here out of simply random() 
 # by 
 
-from spike import Spike
+from spike import Spike, Particle 
 
 spike_queue = []
-
-num_particles = 3
-y_spacing = 5
-
-
+num_particles = 1
 # will have a frame count. if a spike with that init time is present in a particle, 
 # add it to the spike_queue. 
 
-
-
-
 def setup():
+ #   global spike_queue
     size(1000, 1000)
     background(255)
     noStroke()
     frameRate(60)
+   # print(spike_queue)
     particles = [Particle(i) for i in range(num_particles)]
-    map(lambda x: x.populate_spikes, particles)
+    for p in particles:
+        p.populate_assemblies()
+        p.run_snmc()
+        p.populate_wta_and_scoring()
+        spike_queue.append(p.all_spikes[0])
+       
 
-    
-    
-    
-    
-    
   #  spikes.append(Spike(width / 2, height / 2))
     
 def draw():
+    print("drawing")
     background(255)
-    print("yo")
-    for sp in spikes:
+   # print("yo")
+    for sp in spike_queue:
         print(sp.x)
-        sp.move_in_time()
-        sp.display()
-        print(spikes)
-        if sp.spike_finished():
-            spikes.remove(sp)
+        sp.move_in_time(frameCount)
+        sp.display(frameCount)
+        if sp.spike_finished(frameCount):
+            spike_queue.remove(sp)
     
 
     
--- snmc_spike_anim/spike.py
+++ snmc_spike_anim/spike.py
@@ -12,19 +12,21 @@ class Spike():
         self.spike_height = spike_height
         self.spike_stroke = 0
         self.spike_strokeweight = 1
-        self.end_time_axis = 200
+        self.end_time = 200
         
-    def move_in_time(self):
-        self.x = self.x + self.speed
+    def move_in_time(self, fc):
+        if self.init_time <= fc <= self.init_time + self.end_time: 
+            self.x = self.x + self.speed
         
-    def spike_finished(self):
-        return self.x > (self.start_x + self.end_time_axis)
+    def spike_finished(self, fc):
+        return fc > self.init_time + self.end_time 
         
-    def display(self):
-       stroke(self.spike_stroke)
-       strokeWeight(self.spike_strokeweight)
-       line(self.x, self.y, self.x, self.y + self.spike_height)
-       fill(0, 0)
+    def display(self, fc):
+        if self.init_time <= fc <= self.init_time + self.end_time: 
+            stroke(self.spike_stroke)
+            strokeWeight(self.spike_strokeweight)
+            line(self.x, self.y, self.x, self.y + self.spike_height)
+            fill(0, 0)
        
        #rect(self.x, self.y, 20, self.spike_height)  
 
@@ -38,51 +40,84 @@ class Particle:
     def __init__(self, id):
         self.length_of_simulation = 2000
         self.neurons_per_assembly = 2
+        self.num_states = 2 
         self.kq = 5
         self.kp = 10
-        self.num_states = 2 
         # can make this amenable to larger assembly sizes and more latents. 
         # hand code for now. 
         self.assembly_indices = ["q0", "q1", "p0", "p1"]   
-        self.assemblies = {i : [] for i in assembly_indices}
+        
+        self.assemblies = {i : [] for i in self.assembly_indices}
         self.mux = {"q": [], 
                     "p": []}
         self.tik = {"q": [], 
                     "p": []}
         self.accum = {"q": []}
-        self.wta = {"0": [],
-                    "1": []}      
+        self.wta = {0: [],
+                    1: []}      
         self.normalizer = []
         self.resampler = []
         self.all_spikes = []
-        
     
     def populate_assemblies(self):        
         sim_params = [random(0, .2) for i in self.assembly_indices]
        # assembly_params = [s for s in sim_params for _ in (0, self.neurons_per_assembly)]
-        print(assembly_params)
         for ai, sp in zip(self.assembly_indices, sim_params):
             self.assemblies[ai] = [[bernoulli(sp) for i in range(self.length_of_simulation)] 
                                    for n in range(self.neurons_per_assembly)] 
                     
     def run_snmc(self):
-        state = float("Nan")
+        # have to implement counters too. 
+        state = float("NaN")
+        p_tik = 0
+        q_tik = 0
         for t in range(self.length_of_simulation):
             if math.isnan(state):
                 state = self.detect_winner(t)            
-                            
             if not math.isnan(state):
-                self.mux["q"].append(find_spikes_in_assemblies(t, "q" + str(state)))
-                self.mux["p"].append(find_spikes_in_assemblies(t, "q" + str(state)))
+                qmux_spikes = self.find_spikes_in_assemblies(t, "q" + str(state))
+                pmux_spikes = self.find_spikes_in_assemblies(t, "p" + str(state))
                 self.wta[state].append(1)
-                self.wta[!state].append(0)
-                
+                # when more than one state, just add a list here for all !state
+                self.wta[not state].append(0)
+                total_p_assembly_spikes = pmux_spikes + self.find_spikes_in_assemblies(t, "p" + str(int(not state)))
+                total_q_assembly_spikes = qmux_spikes + self.find_spikes_in_assemblies(t, "q" + str(int(not state)))
+                p_tik += total_p_assembly_spikes
+                if p_tik <= self.kp:
+                    self.mux["p"].append(pmux_spikes)
+                    if p_tik == self.kp:
+                        self.tik["p"].append(1)
+                    else:
+                        self.tik["p"].append(0)
+                else: 
+                    self.mux["p"].append(0)  
+                    self.tik["p"].append(0)                     
+                q_tik += qmux_spikes
+                if q_tik <= self.kq:
+                    self.mux["q"].append(qmux_spikes)
+                    self.accum["q"].append(total_q_assembly_spikes)
+                    if q_tik == self.kq:
+                        self.tik["q"].append(1)
+                    else:
+                        self.tik["q"].append(0)                    
+                else: 
+                    self.mux["q"].append(0)
+                    self.accum["q"].append(0)
+                    self.tik["q"].append(0)
+                if p_tik > self.kp and q_tik > self.kq:
+                    p_tik = 0
+                    q_tik = 0
+                    state = float("NaN")
+                                    
             else:
                 self.mux["q"].append(0)
                 self.mux["p"].append(0)
                 self.wta[0].append(0)
                 self.wta[1].append(0)
-            
+                self.tik["p"].append(0)
+                self.tik["q"].append(0)
+                self.accum["q"].append(0)
+     
           
     def detect_winner(self, time):
         spike_in_0 = self.find_spikes_in_assemblies(time, "q0")
@@ -91,33 +126,39 @@ class Particle:
             return 0 
         elif spike_in_1 and not spike_in_0:
             return 1
-        elif spike_in_1 and spike_in_2:
+        elif spike_in_1 and spike_in_0:
             return bernoulli(.5) 
         else:
             return float("NaN")
         
     def find_spikes_in_assemblies(self, time, assembly):
-        spikes = sum([self.assemblies[assembly][i][time] for i in self.neurons_per_assembly])
+        spikes = sum([self.assemblies[assembly][i][time] for i in range(self.neurons_per_assembly)])
         return spikes
         
-    def populate_spikes(self):
+    def populate_wta_and_scoring(self):
     
         spike_height = 4
         spacing = 1
         cortex_x = 300
         cortex_y = 300
-        # elements have to be arranged in y-order        
-        elements = [self.wta[0], self.wta[1]] + [self.assemblies[pq + pq_ind][n_id] for pq in ["p", "q"] for n_ind in range(
-                                                 self.neurons_per_assembly) for pq_ind in range(self.num_states)] + [self.mux["p"], self.mux["q"]]
-    
+        # elements have to be arranged in y-order   
+        wtas = [self.wta[0], self.wta[1]]
+        assemblies = [self.assemblies[pq + str(pq_ind)][n_id] for pq in ["p", "q"] for n_id in range(
+                       self.neurons_per_assembly) for pq_ind in range(self.num_states)]
+        scoring = [self.mux["p"], self.mux["q"], self.tik["p"], self.tik["q"], self.accum["q"]]
+        elements = wtas + assemblies + scoring 
         sp_train_ylocs = range(cortex_y, cortex_y + ((spike_height + spacing) * len(elements)))
-        
         for i, elem in enumerate(elements):
-            assign_spikes(elem, cortex_x, sp_train_ylocs(i), spike_height)
-        
+            self.assign_spikes(elem, cortex_x, sp_train_ylocs[i], spike_height)        
                     
     def assign_spikes(self, spikelist, x, y, spike_height):
-        self.all_spikes += [Spike(x, y, t, spike_height) if spikelist[t] != 0 for t in range(len(self.length_of_simulation))]
+        for t in range(self.length_of_simulation):
+            if spikelist[t] != 0:
+              #  print(spikelist[t])
+                self.all_spikes.append(Spike(x, y, t, spike_height))
+        
+        
+#        self.all_spikes += [Spike(x, y, t, spike_height) for t in range(len(self.length_of_simulation)) if spikelist[t] != 0]
           
                 
         
